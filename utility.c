c4d9156340b0ffc70df0c7475f196eed6dcbc313
alex2639 <mr.e_000@hotmail.com>
2017-02-20 23:17:49 -0500

    Added command functions

--- utility.c
+++ utility.c
@@ -1,14 +1,10 @@
 /*
  * MyShell Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2017, <GROUP MEMBERS>
+ * Copyright (C) 2017, Alex, Damon
  * All rights reserved.
- * 
+ *
  */
-
-
-#define _GNU_SOURCE //for scandir
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -17,63 +13,97 @@
 #include <string.h>
 #include "utility.h"
 
-
 // Define your utility functions here, these will most likely be functions that you call
 // in your myshell.c source file
 
-
-void clear()
-{
-    printf("\033[2J");
-}
-
-void list_directory(char *directory)
-{
-
-    if(directory == NULL)
+void cd(){
+  char *ar = strtok(NULL, "");
+  if(ar != NULL)
+  {
+    strcpy(arg, ar);
+    if(chdir(arg) != 0)
     {
-        directory = "./";
-    }
-
-    DIR *dp;
-    struct dirent **file_names;   
-    int number_of_files;  
-    dp = opendir (directory);
-
-    number_of_files = scandir(directory, &file_names, NULL, alphasort);
-
-    if (dp != NULL)
-    {
-        //while (file_names = readdir (dp))
-        for(int i = 0; i < number_of_files; i ++)
-        {
-            puts (file_names[i]->d_name);
-        }
-
-        (void) closedir (dp);
+      //Can't find the directory specified'
+      printf("can't find %s\n", arg);
     }
     else
     {
-        perror ("Couldn't open the directory");
+      //Set new pwd
+      getcwd(pwd, sizeof(pwd));
     }
-    free(file_names);
+  }
+  else
+  {
+    //print pwd and a new line
+    printf("%s\n", pwd);
+  }
 }
 
+void clear_shell(){
+  clear();
+}
 
-void cd(char *directory, char *pwd)
-{
-        if(directory != NULL)
-        {
-          //  strcpy(arg, ar);
-            if(chdir(directory) != 0)
-            {
-                //Can't find the directory specified'
-                printf("can't find %s\n", directory);
-            }
-        }
-        else
-        {
-            //print pwd and a new line
-            printf("%s\n", pwd);
-        }
+void echo(char *arg,FILE *file){
+  char *ar = strtok(NULL, "");
+  if(ar != NULL)
+  {
+    strcpy(arg, ar);
+    if(file=NULL)
+    {
+      printf("%s ", arg);
+    }
+    else
+    {
+      fprintf(file, "%s ", arg);
+    }
+  }
+  if(file=NULL)
+  {
+    printf("\n");
+  }
+  else
+  {
+    fprintf(file,"\n");
+  }
+}
+
+void environ(char *pwd,FILE *file){
+  const char* s = getenv("PATH");
+  getcwd(pwd, sizeof(pwd));
+  if(file=NULL)
+  {
+    printf("SHELL :%s\n",(s!=NULL)? s : "getenv returned NULL");
+    printf("PWD: %s\n",pwd);
+  }
+  else
+  {
+    fprintf(file,"SHELL :%s\n",(s!=NULL)? s : "getenv returned NULL");
+    fprintf(file,"PWD: %s\n",pwd);
+  }
+
+}
+
+void dir(){
+  char *ar = strtok(NULL, "");
+  list_directory(ar);
+}
+
+void help(){
+  char *ar = strtok(NULL, "");
+  if(ar != NULL)
+  {
+    char *cmd=strcat("man ", ar);
+    int status=system(cmd);
+  }
+}
+
+void pause(){
+  while(1)
+  {
+    char c = getchar();
+    if(c == '\n')//if ENTER key is pressed
+    {
+      break;
+    }
+  }
 }
--- utility.h
+++ utility.h
@@ -1,20 +1,22 @@
 /*
  * MyShell Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2017, <GROUP MEMBERS>
+ * Copyright (C) 2017, Alex, Damon
  * All rights reserved.
- * 
+ *
  */
 #ifndef UTILITY_H_
 #define UTILITY_H_
 
-// Include your relevant functions declarations here they must start with the 
+// Include your relevant functions declarations here they must start with the
 // extern keyword such as in the following example:
 // extern void display_help(void);
-
-
-extern void list_directory(char *directory);
-extern void clear();
-extern void cd(char *directory, char *pwd);
+extern void cd(void);
+extern void clear_shell(void);
+extern void echo(char *arg,FILE *file);
+extern void environ(char *pwd,FILE *file);
+extern void dir(void);
+extern void help(void);
+extern void pause(void);
 
 #endif /* UTILITY_H_ */
