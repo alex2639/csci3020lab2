b78f33295019ce501eddc938e183f0ec8a83f08e
alex2639 <mr.e_000@hotmail.com>
2017-02-20 23:17:11 -0500

    Added redirect i/o

--- myshell.c
+++ myshell.c
@@ -1,7 +1,7 @@
 /*
  * MyShell Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2017, Alex, Damon
+ * Copyright (C) 2017, Alex, Damon, Irfaan
  * All rights reserved.
  *
  */
@@ -33,6 +33,303 @@ int main(int argc, char *argv[])
     printf("%s> ", pwd);
 
     // Parse the commands provided using argc and argv
+    if(argc==1){
+      FILE *file=fopen(argv[1],"r");
+      char *line;
+      while(fscanf(file,line)!=EOF){
+        if (strcmp(line, "cd") == 0)
+        {
+            cd()
+        }
+
+        else if(strcmp(line, "clr") == 0)
+        {
+            clear_shell();
+        }
+
+        // quit command -- exit the shell
+        else if (strcmp(line, "quit") == 0)
+        {
+            return EXIT_SUCCESS;
+        }
+
+        else if (strcmp(line, "echo")==0)
+        {
+            echo(arg);
+        }
+
+        else if (strcmp(line, "environ")==0)
+        {
+            environ(pwd);
+        }
+
+        else if (strcmp(line, "dir")==0)
+        {
+            dir();
+        }
+
+        else if (strcmp(line, "help")==0)
+        {
+            help();
+        }
+
+        else if (strcmp(line,"pause")==0)
+        {
+            pause();
+        }
+
+        // Unsupported command
+        else
+        {
+            fputs("Unsupported command, use help to display the manual\n", stderr);
+        }
+      }
+      return EXIT_SUCCESS;
+    }else if(argc==2)
+    {
+      if(strcmp(argv[1],"<")==0)
+      {
+        FILE *file=fopen(argv[2],"r");
+        char *line;
+        while(fscanf(file,line)!=EOF)
+        {
+          if (strcmp(line, "cd") == 0)
+          {
+              cd();
+          }
+
+          else if(strcmp(line, "clr") == 0)
+          {
+              clear_shell();
+          }
+
+          // quit command -- exit the shell
+          else if (strcmp(line, "quit") == 0)
+          {
+              return EXIT_SUCCESS;
+          }
+
+          else if (strcmp(line, "echo")==0)
+          {
+              echo(arg,NULL);
+          }
+
+          else if (strcmp(line, "environ")==0)
+          {
+              environ(pwd,NULL);
+          }
+
+          else if (strcmp(line, "dir")==0)
+          {
+              dir();
+          }
+
+          else if (strcmp(line, "help")==0)
+          {
+              help();
+          }
+
+          else if (strcmp(line,"pause")==0)
+          {
+              pause();
+          }
+
+          // Unsupported command
+          else
+          {
+              fputs("Unsupported command, use help to display the manual\n", stderr);
+          }
+      }
+      else if(strcmp(argv[1],">")==0)
+      {
+        FILE *file=fopen(argv[2],"w");
+        // Perform an infinite loop getting command input from users
+        while (fgets(buffer, BUFFER_LEN, stdin) != NULL)
+        {
+            // Perform string tokenization to get the command and argument
+            char *pos;
+            if ((pos=strchr(buffer, '\n')) != NULL)
+            {
+                *pos = '\0';
+            }
+            strcpy(command, strtok(buffer, " "));
+
+            if (strcmp(command, "cd") == 0)
+            {
+                cd();
+            }
+
+            else if(strcmp(command, "clr") == 0)
+            {
+                clear_shell();
+            }
+
+            // quit command -- exit the shell
+            else if (strcmp(command, "quit") == 0)
+            {
+                return EXIT_SUCCESS;
+            }
+
+            else if (strcmp(command, "echo")==0)
+            {
+                echo(arg,file);
+            }
+
+            else if (strcmp(command, "environ")==0)
+            {
+                environ(pwd,file);
+            }
+
+            else if (strcmp(command, "dir")==0)
+            {
+                dir();
+            }
+
+            else if (strcmp(command, "help")==0)
+            {
+                help();
+            }
+
+            else if (strcmp(command,"pause")==0)
+            {
+                pause();
+            }
+
+            // Unsupported command
+            else
+            {
+                fputs("Unsupported command, use help to display the manual\n", stderr);
+            }
+            printf("%s>", pwd);
+        }
+        return EXIT_SUCCESS;
+      }
+      else if(strcmp(argv[1],">>")==0)
+      {
+        FILE *file=fopen(argv[2],"wa");
+        // Perform an infinite loop getting command input from users
+        while (fgets(buffer, BUFFER_LEN, stdin) != NULL)
+        {
+            // Perform string tokenization to get the command and argument
+            char *pos;
+            if ((pos=strchr(buffer, '\n')) != NULL)
+            {
+                *pos = '\0';
+            }
+            strcpy(command, strtok(buffer, " "));
+
+            if (strcmp(command, "cd") == 0)
+            {
+                cd();
+            }
+
+            else if(strcmp(command, "clr") == 0)
+            {
+                clear_shell();
+            }
+
+            // quit command -- exit the shell
+            else if (strcmp(command, "quit") == 0)
+            {
+                return EXIT_SUCCESS;
+            }
+
+            else if (strcmp(command, "echo")==0)
+            {
+                echo(arg,file);
+            }
+
+            else if (strcmp(command, "environ")==0)
+            {
+                environ(pwd,file);
+            }
+
+            else if (strcmp(command, "dir")==0)
+            {
+                dir();
+            }
+
+            else if (strcmp(command, "help")==0)
+            {
+                help();
+            }
+
+            else if (strcmp(command,"pause")==0)
+            {
+                pause();
+            }
+
+            // Unsupported command
+            else
+            {
+                fputs("Unsupported command, use help to display the manual\n", stderr);
+            }
+            printf("%s>", pwd);
+        }
+        return EXIT_SUCCESS;
+      }
+    }
+    else if(argc==4)
+    {
+      FILE *in_file=fopen(argv[2],"r");
+      if(strcmp(argv[3],">")==0)
+      {
+        FILE *out_file=fopen(argv[4],"w");
+      }
+      else
+      {
+        FILE *out_file=fopen(argv[4],"wa");
+      }
+      char *line;
+      while(fscanf(in_file,line)!=EOF)
+      {
+        if (strcmp(line, "cd") == 0)
+        {
+            cd();
+        }
+
+        else if(strcmp(line, "clr") == 0)
+        {
+            clear_shell();
+        }
+
+        // quit command -- exit the shell
+        else if (strcmp(line, "quit") == 0)
+        {
+            return EXIT_SUCCESS;
+        }
+
+        else if (strcmp(line, "echo")==0)
+        {
+            echo(arg,out_file);
+        }
+
+        else if (strcmp(line, "environ")==0)
+        {
+            environ(pwd,out_file);
+        }
+
+        else if (strcmp(line, "dir")==0)
+        {
+            dir();
+        }
+
+        else if (strcmp(line, "help")==0)
+        {
+            help();
+        }
+
+        else if (strcmp(line,"pause")==0)
+        {
+            pause();
+        }
+
+        // Unsupported command
+        else
+        {
+            fputs("Unsupported command, use help to display the manual\n", stderr);
+        }
+    }
 
     // Perform an infinite loop getting command input from users
     while (fgets(buffer, BUFFER_LEN, stdin) != NULL)
@@ -47,18 +344,13 @@ int main(int argc, char *argv[])
 
         if (strcmp(command, "cd") == 0)
         {
-            char *ar = strtok(NULL, "");
-            cd(ar, pwd);
-            getcwd(pwd, sizeof(pwd));
+            cd();
         }
 
         else if(strcmp(command, "clr") == 0)
         {
-           clear();
+            clear_shell();
         }
-
-
-        // other commands here...
 
         // quit command -- exit the shell
         else if (strcmp(command, "quit") == 0)
@@ -68,51 +360,27 @@ int main(int argc, char *argv[])
 
         else if (strcmp(command, "echo")==0)
         {
-          //printf("%s\n", buffer);
-          char *ar = strtok(NULL, "");
-          if(ar != NULL)
-          {
-            strcpy(arg, ar);
-            printf("%s ", arg);
-          }
-          printf("\n");
+            echo(arg,NULL);
         }
 
         else if (strcmp(command, "environ")==0)
         {
-          const char* s = getenv("PATH");
-          printf("SHELL :%s\n",(s!=NULL)? s : "getenv returned NULL");
-          getcwd(pwd, sizeof(pwd));
-          printf("PWD: %s\n",pwd);  
+            environ(pwd,NULL);
         }
 
         else if (strcmp(command, "dir")==0)
         {
-            char *ar = strtok(NULL, "");
-            list_directory(ar);
+            dir();
         }
 
         else if (strcmp(command, "help")==0)
         {
-            char *ar = strtok(NULL, "");
-            if(ar != NULL)
-            {
-            
-            char *cmd=strcat("man ", ar);
-            int status=system(cmd);
-          }
+            help();
         }
 
         else if (strcmp(command,"pause")==0)
         {
-          while(1)
-          {
-            char c = getchar();
-            if(c == '\n')//if ENTER key is pressed
-            {
-              break;
-            }
-          }
+            pause();
         }
 
         // Unsupported command
--- myshell.h
+++ myshell.h
@@ -1,14 +1,14 @@
 /*
  * MyShell Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2017, <GROUP MEMBERS>
+ * Copyright (C) 2017, Alex, Damon
  * All rights reserved.
- * 
+ *
  */
 #ifndef MYSHELL_H_
 #define MYSHELL_H_
 
-// Include your relevant functions declarations here they must start with the 
+// Include your relevant functions declarations here they must start with the
 // extern keyword such as in the following example:
 // extern void display_help(void);
 
